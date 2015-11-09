cf3482b7b311616b58d6856088779bae399ebeae
Austin Kregel <austin@kbcomp.co>
2015-11-09 16:45:59 -0500

    Add uuid to base model

--- src/AuthLogin/Http/Controllers/Auth/AuthController.php
+++ src/AuthLogin/Http/Controllers/Auth/AuthController.php
@@ -15,7 +15,6 @@ class AuthController extends Controller
     /**
      * Create a new authentication controller instance.
      *
-     * @return void
      */
     public function __construct()
     {
@@ -218,10 +217,21 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = new User;
+        $user->fill([
+            'uuid' => $this->uuid(openssl_random_pseudo_bytes(16)),
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+    }
+    private function uuid($data)
+    {
+        assert(strlen($data) == 16);
+
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 }
