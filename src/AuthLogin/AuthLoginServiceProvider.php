d9a1c62edc55e8341fa7419b9f421f226342b8f7
Austin Kregel <austinkregel@gmail.com>
2016-06-02 14:28:16 -0400

    Fix bug mentioned in #3

--- src/AuthLogin/AuthLoginServiceProvider.php
+++ src/AuthLogin/AuthLoginServiceProvider.php
@@ -50,7 +50,7 @@ class AuthLoginServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->register(\Tymon\JWTAuth\Providers\LaravelServiceProvider::class);
+        $this->app->register(\Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class);
     }
 
     /**
