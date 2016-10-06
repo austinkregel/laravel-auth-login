68a4d735e4413a572b5ec720c10158dfbc9a7b1c
Austin <austinkregel@users.noreply.github.com>
2016-10-06 13:12:01 -0400

    Update password.blade.php

--- src/resources/views/security/password.blade.php
+++ src/resources/views/security/password.blade.php
@@ -3,32 +3,37 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="card">
-                    <h2>Update Password</h2>
+            <div class="col-md-4">
+                @include('auth-login::shared.menu')
+            </div>
+            <div class="col-md-8">
+                <div class="panel panel-default" style="padding:1rem;">
+                    <h2 style="margin-top:0;">Update Password</h2>
 
                     <div class="panel-body">
-                        <form class="form-horizontal" role="form">
+                        <form class="form-horizontal" role="form" action="{{ route('auth-login::update-security') }}" method="post">
+                            {!! method_field('put') !!}
+                            {!! csrf_field() !!}
                             <div class="form-group">
                                 <label class="col-md-4 control-label">Current Password</label>
                                 <div class="col-md-6">
-                                    <input type="password" class="form-control">
+                                    <input type="password" class="form-control" name="old_password">
                                 </div>
                             </div>
 
                             <div class="form-group">
                                 <label class="col-md-4 control-label">New Password</label>
                                 <div class="col-md-6">
-                                    <input type="password" class="form-control">
+                                    <input type="password" class="form-control" name="password">
                                 </div>
-                            </div>
+                            </div><!--v-component-->
 
                             <div class="form-group">
                                 <label class="col-md-4 control-label">Confirm Password</label>
                                 <div class="col-md-6">
-                                    <input type="password" class="form-control">
+                                    <input type="password" class="form-control" name="confirm_password">
                                 </div>
-                            </div>
+                            </div><!--v-component-->
 
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
